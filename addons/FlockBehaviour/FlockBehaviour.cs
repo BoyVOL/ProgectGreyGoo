98fe28cfdaf381017707cf47fff835c5b2d44031
BoyVOL <vladislav_limonov@mail.ru>
2025-05-27 23:01:47 +0900

    Создана Тестовая Нода

--- addons/FlockBehaviour/FlockBehaviour.cs
+++ addons/FlockBehaviour/FlockBehaviour.cs
@@ -8,6 +8,9 @@ public partial class FlockBehaviour : EditorPlugin
 	public override void _EnterTree()
 	{
 		// Initialization of the plugin goes here.
+        var script = GD.Load<Script>("res://addons/FlockBehaviour/FlockNode/FlockNode.cs");
+        var texture = GD.Load<Texture2D>("res://addons/FlockBehaviour/FlockNode/FlockIcon.png");
+        AddCustomType("FlockNode", "Node", script, texture);
 		GD.Print("Flock Behaviour Loaded");
 	}
 
