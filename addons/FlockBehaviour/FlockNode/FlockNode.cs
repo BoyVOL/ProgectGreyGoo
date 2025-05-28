8767374bd24019692b51fdeee2bd285295231c62
BoyVOL <vladislav_limonov@mail.ru>
2025-05-28 11:00:15 +0900

    Testing avareness of Flick node's children

--- addons/FlockBehaviour/FlockNode/FlockNode.cs
+++ addons/FlockBehaviour/FlockNode/FlockNode.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 
 public partial class FlockNode : Node
@@ -12,5 +13,17 @@ public partial class FlockNode : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame. Test
 	public override void _Process(double delta)
 	{
+		base._Process(delta);
 	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		Godot.Collections.Array<Node> Test = this.GetChildren(false);
+		foreach (Node2D node in Test)
+		{
+			GD.Print(node.Position);
+		}
+    }
+
 }
