60d863c31dfc8d88263e8cc628943c57c410953e
BoyVOL <vladislav_limonov@mail.ru>
2025-06-09 11:28:26 +0900

    Добавлена нода кликера

--- addons/ClickOrders/ClickOrders.cs
+++ addons/ClickOrders/ClickOrders.cs
@@ -7,12 +7,15 @@ public partial class ClickOrders : EditorPlugin
 {
 	public override void _EnterTree()
 	{
-		// Initialization of the plugin goes here.
+        var script = GD.Load<Script>("res://addons/ClickOrders/Clicker/Clicker.cs");
+        var texture = GD.Load<Texture2D>("res://addons/ClickOrders/Clicker/ClickerIcon.png");
+        AddCustomType("Clicker", "Node", script, texture);
+		GD.Print("Click Orders Loaded");
 	}
 
 	public override void _ExitTree()
 	{
-		// Clean-up of the plugin goes here.
+		GD.Print("Click Orders Unloaded");
 	}
 }
 #endif
