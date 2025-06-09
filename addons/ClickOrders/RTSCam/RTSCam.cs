ac85f5d3edf5aebc542f0ed29b8814d121fcb0c3
BoyVOL <vladislav_limonov@mail.ru>
2025-06-09 12:40:22 +0900

    Дебаг класса.

--- addons/ClickOrders/RTSCam/RTSCam.cs
+++ addons/ClickOrders/RTSCam/RTSCam.cs
@@ -1,6 +1,12 @@
 using Godot;
 using System;
 
-public partial class Clicker : Camera2D
+public partial class RTSCam : Camera2D
 {
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        GD.Print(@event);
+    }
+
 }
