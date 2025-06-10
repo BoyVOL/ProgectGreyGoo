ec1849ea967d8c1b4dbe30a0885a90492bc98628
BoyVOL <vladislav_limonov@mail.ru>
2025-06-10 14:36:03 +0900

    Зум и координаты через клик по окну

--- addons/ClickOrders/RTSCam/RTSCam.cs
+++ addons/ClickOrders/RTSCam/RTSCam.cs
@@ -3,10 +3,47 @@ using System;
 
 public partial class RTSCam : Camera2D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+
+
+    [Export]
+    float ZoomFactor = 1.1f;
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        GD.Print(@event);
+        if (@event is InputEventMouseButton)
+        {
+            InputEventMouseButton MBEvent = (InputEventMouseButton)@event;
+            GD.Print(GlobalPosition);
+            GD.Print(GetGlobalMousePosition());
+            Scroll((InputEventMouseButton)@event);
+        }
+    }
+
+    
+    public Vector2 Centralised(Vector2 Position)
+    {
+        Rect2 port = GetViewportRect();
+        Vector2 ViewportSize = port.End - port.Position;
+        return Position - ViewportSize / 2;
+    }
+
+    public void Scroll(InputEventMouseButton @event)
+    {
+        if (@event.Pressed)
+        {
+            if (@event.ButtonIndex == MouseButton.WheelDown)
+            {
+                Zoom /= ZoomFactor;
+            }
+            if (@event.ButtonIndex == MouseButton.WheelUp)
+            {
+                Zoom *= ZoomFactor;
+            }
+        }
     }
 
 }
