181213de0fe703ab8a304454d35ad65fcd03024f
BoyVOL <vladislav_limonov@mail.ru>
2025-06-13 20:36:42 +0900

    Тестирование UI ивентов

---
+++ Scenes/TestScenes/MarginContainer.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class MarginContainer : Godot.MarginContainer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _GuiInput(InputEvent @event)
+	{
+		base._GuiInput(@event);
+		//GD.Print("GUI");
+    }
+
+}
